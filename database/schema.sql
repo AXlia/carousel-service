31f9708a1fb159317c630c3fe7895d6a40aebf5a
Michelle-Ward <Mward352@gmail.com>
2021-01-14 12:20:17 -0800

    update dependencies for refactoring to use sequelize

--- database/schema.sql
+++ database/schema.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE IF NOT EXISTS homes;
+CREATE DATABASE IF NOT EXISTS listings;
 USE listings;
 
-CREATE TABLE IF NOT EXISTS listings (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  address VARCHAR(80),
-  neighbourhood VARCHAR(80),
-  city VARCHAR(80),
-  state VARCHAR(80),
-  price VARCHAR(80),
-  bedrooms INT,
-  bathrooms INT,
-  sqft VARCHAR(80),
-  realtor VARCHAR(80),
-  newTag TINYINT,
-  liked TINYINT,
-);
+-- CREATE TABLE IF NOT EXISTS homes (
+--   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   address VARCHAR(80),
+--   neighbourhood VARCHAR(80),
+--   city VARCHAR(80),
+--   state VARCHAR(80),
+--   price VARCHAR(80),
+--   bedrooms INT,
+--   bathrooms INT,
+--   sqft VARCHAR(80),
+--   realtor VARCHAR(80),
+--   newTag TINYINT,
+--   liked TINYINT,
+-- );
 
 -- CREATE TABLE IF NOT EXISTS tags (
 --   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,

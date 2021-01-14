c5db3901aa063c004efea2252f9d96da5b26c715
Michelle-Ward <Mward352@gmail.com>
2021-01-14 11:51:49 -0800

    refactor to use sequelize

--- database/index.js
+++ database/index.js
@@ -1,16 +1,83 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'student',
+//   password: 'student',
+//   database: 'homes',
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.log('could not connect to mysql db, ', err);
+//   } else {
+//     console.log('connected to mysql db');
+//   }
+// });
+
+// module.exports = connection;
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('listings', 'student', 'student', {
   host: 'localhost',
-  user: 'student',
-  password: 'student',
-  database: 'homes',
+  dialect: 'mysql',
+  define: { timestamps: false },
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log('could not connect to mysql db, ', err);
-  } else {
-    console.log('connected to mysql db');
-  }
+const Home = sequelize.define('home', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+  neighborhood: {
+    type: DataTypes.STRING,
+  },
+  city: {
+    type: DataTypes.STRING,
+  },
+  state: {
+    type: DataTypes.STRING,
+  },
+  price: {
+    type: DataTypes.STRING,
+  },
+  bedrooms: {
+    type: DataTypes.INTEGER,
+  },
+  bathrooms: {
+    type: DataTypes.INTEGER,
+  },
+  sqft: {
+    type: DataTypes.STRING,
+  },
+  realtor: {
+    type: DataTypes.STRING,
+  },
+  newTag: {
+    type: DataTypes.BOOLEAN,
+  },
+  like: {
+    type: DataTypes.BOOLEAN,
+  },
 });
+
+sequelize.sync();
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
+
+module.export = {
+  Home,
+  sequelize,
+};

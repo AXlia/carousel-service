de9e2dd00bf5fe153769ad4d28c6fc702d869437
Michelle-Ward <Mward352@gmail.com>
2021-01-14 12:32:08 -0800

    create API GET request route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const controller = require('./controller.js');
 
 const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
+
+app.get('/api/homes', controller.getListings);
 
 app.listen(port, (err) => {
   if (err) {

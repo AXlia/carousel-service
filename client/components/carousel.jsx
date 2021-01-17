e52daebdb443570e67fe095f121e5477fc845275
Michelle-Ward <Mward352@gmail.com>
2021-01-16 17:51:55 -0800

    write approximate button divs and end card placeholder

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -4,7 +4,12 @@ import Item from './item.jsx';
 
 const Carousel = (props) => (
   <div>
+    <button>Left Arrow</button>
     {props.homes.map((home) => <Item home={home} key={home._id}/>)}
+    <div>
+      <p>This is the end of carousel see more div that will always be at the end</p>
+    </div>
+    <button>Right Arrow</button>
   </div>
 );
 

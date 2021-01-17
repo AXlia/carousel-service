dff4c6d180f895520746cfc027619f8bd7fd9d24
Michelle-Ward <Mward352@gmail.com>
2021-01-16 18:49:07 -0800

    create styled image component for thumbnails

--- client/components/item.jsx
+++ client/components/item.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable */
 import React from 'react';
+import styled from 'styled-components'
+
+// const Image = ({home}) => {
+//   <img src={home.imageUrl} />
+// }
+const StyledImg = styled.img`
+  width: 224px;
+  height: 160px;
+`;
 
 const Item = ({ home }) => (
   <div>
-    <img src={home.imageUrl} />
+    <StyledImg src={home.imageUrl} />
     <span>{home.bedrooms}bd </span>
     <span>{home.bathrooms}ba </span>
     <span>{home.sqft} sqft</span>
-    <h2>{home.address}</h2>
+    <p>{home.address}</p>
     <p>{home.neighborhood}, {home.city} {home.state}</p>
     <hr></hr>
     <p>{home.realtor}</p>
@@ -15,3 +24,10 @@ const Item = ({ home }) => (
 );
 
 export default Item;
+//the icons for bedrooms bathrooms and sqft are svg
+//they are held in multiple divs
+//big div- media block
+//inner div each span and svg
+
+
+//hr is actually a border top of the realtor div

0bea5266645751e8bbf0e643ce81b444a57f2280
Michelle-Ward <Mward352@gmail.com>
2021-01-17 16:38:25 -0800

    add arrow icons to button elements

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -2,25 +2,32 @@
 import React from 'react';
 import Item from './item.jsx';
 import styled from 'styled-components';
+import {ArrowIosForwardOutline} from '@styled-icons/evaicons-outline/ArrowIosForwardOutline';
+import {ArrowIosBackOutline} from '@styled-icons/evaicons-outline/ArrowIosBackOutline';
 
 const Arrow = styled.div`
-  border: 0px;
-  width: 50px;
-  height: 50px;
-  border-radius: 100px;
-  background-color: grey;
+  display: flex;
+  padding: 1px;
+  border: 1px solid rgb(232, 233, 234);
+  background-color: rgb(255, 255, 255);
+  width: 32px;
+  height: 32px;
+  border-radius: 24px;
   text-align: center;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  transition: box-shadow 0.1s ease 0s, color 0.1s ease 0s, border-color 0.2s ease 0s, background-color 0.2s ease 0s;
 `
 
 const Carousel = (props) => (
   <div>
-    <Arrow>{'<'}</Arrow>
+    <Arrow><ArrowIosForwardOutline size="28"/></Arrow>
     {props.homes.map((home) => <Item home={home} key={home._id}/>)}
     <div>
-
       <p>This is the end of carousel see more div that will always be at the end</p>
     </div>
-    <button>Right Arrow</button>
+    <Arrow><ArrowIosBackOutline size="28"/></Arrow>
   </div>
 );
 
